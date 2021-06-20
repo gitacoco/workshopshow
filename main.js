d9ae064c20e97f8f8416dabac40a477e20a36ab4
zjhch123 <416836508@qq.com>
2021-06-20 12:04:27 +0800

    pull

--- main.js
+++ main.js
@@ -51,9 +51,18 @@ var swiper = new Swiper(".swiper-container", {
   },
   on: {
     click: function (swiper, e) {
-      if (typeof this.clickedIndex !== "undefined") {
+      if (typeof this.clickedIndex !== "undefined" && this.clickedIndex !== this.activeIndex && !pulled) {
         this.slideTo(this.clickedIndex);
+        setTimeout(() => {
+          pull();
+        }, 500);
       }
+    },
+    slideChangeTransitionStart: function() {
+      $(".swiper-slide-active").removeClass('enable-pull');
+    },
+    slideChangeTransitionEnd: function() {
+      $(".swiper-slide-active").addClass('enable-pull');
     },
   },
 });
@@ -62,15 +71,80 @@ $(window).on('hashchange', () => {
   $('video')[0].pause();
 });
 
-setInterval(() => {
-  const groupId = $(".swiper-slide-active").prev().prev().attr('data-group');
+let intervalId = null;
+let pulled = false;
+const showTitleForGroup = () => {
+  if (intervalId !== null) { return; }
+  intervalId = setInterval(() => {
+    const groupId = $(".swiper-slide-active").prev().prev().attr('data-group');
+    $(`.title img`).hide();
+    $(`.title img.group${groupId}-title`).show();
+    const prevGroupId = $(".swiper-slide-active").prev().prev().prev().attr('data-group');
+  
+    if (groupId !== prevGroupId) {
+      $(".swiper-slide-active").prev().prev().find('.titleImage').hide();
+    } else {
+      $(".swiper-slide").prev().prev().find('.titleImage').show();
+    }
+  }, 8);
+}
+const showTitleForActive = () => {
+  clearInterval(intervalId);
+  intervalId = null;
+
+  const groupId = $(".swiper-slide-active").attr('data-group');
   $(`.title img`).hide();
   $(`.title img.group${groupId}-title`).show();
-  const prevGroupId = $(".swiper-slide-active").prev().prev().prev().attr('data-group');
+  $(".swiper-slide-active").find('.titleImage').hide();
+};
+const pull = () => {
+  const width = document.documentElement.clientWidth / 5;
+  $('.swiper-slide-active').css({
+    'z-index': 5,
+  });
+  $('.swiper-slide-active').find('.swiper-slide-container').addClass('active');
+  $('.swiper-slide-active').find('.swiper-slide-container').css({
+    transform: `translateX(-${width * 1.5}px)`,
+  });
+  $('.swiper-slide-active').next().find('.swiper-slide-container').addClass('next');
+  $('.swiper-slide-active').next().next().find('.swiper-slide-container').addClass('next');
+  $('.swiper-slide-active').prev().find('.swiper-slide-container').addClass('prev');
+  $('.swiper-slide-active').prev().prev().find('.swiper-slide-container').addClass('prev');
+  showTitleForActive();
+  $('.swiper-slide-active').find('.pulled-content').show();
+  $('#control').fadeOut();
+  $('.swiper-slide').addClass('swiper-no-swiping');
+  swiper.simulateTouch = false;
+  swiper.update();
+  pulled = true;
+};
+const clearPull = () => {
+  $('.swiper-slide-active').find('.swiper-slide-container').removeClass('active');
+  $('.swiper-slide-active').css({
+    'z-index': 1,
+  });
+  $('.swiper-slide-active').find('.swiper-slide-container').css({
+    transform: `translateX(0px)`,
+  });
+  $('.swiper-slide-active').next().find('.swiper-slide-container').removeClass('next');
+  $('.swiper-slide-active').next().next().find('.swiper-slide-container').removeClass('next');
+  $('.swiper-slide-active').prev().find('.swiper-slide-container').removeClass('prev');
+  $('.swiper-slide-active').prev().prev().find('.swiper-slide-container').removeClass('prev');
+  showTitleForGroup();
+  $('#control').fadeIn();
+  $('.swiper-slide').removeClass('swiper-no-swiping');
+  swiper.simulateTouch = true;
+  swiper.update();
+  pulled = false;
+};
 
-  if (groupId !== prevGroupId) {
-    $(".swiper-slide-active").prev().prev().find('.titleImage').hide();
-  } else {
-    $(".swiper-slide").prev().prev().find('.titleImage').show();
-  }
-}, 8);
+$('body').on('click', '.swiper-slide-active.enable-pull h1', function() {
+  pull()
+});
+
+$('body').on('click', '.back', (e) => {
+  e.preventDefault();
+  clearPull();
+});
+
+showTitleForGroup();
